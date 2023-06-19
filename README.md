2f2e7f123c8ccb7346b06fd3928c3963e123dc59
Hakeem <12151899q@gmail.com>
2023-06-19 13:07:20 -0700

    added the criteria for password generator

---
+++ README.md
@@ -0,0 +1,21 @@
+# Password Generator
+
+## Description
+This is a password generator application that allows users to generate secure passwords based on their desired criteria. It provides flexibility in selecting password length and character types.
+
+## Features
+- Generate a random password based on user-selected criteria.
+- Allow users to specify the length of the password.
+- Allow users to include or exclude lowercase, uppercase, numeric, and special characters.
+- Validate user input to ensure a valid length is provided and at least one character type is selected.
+- Display the generated password.
+
+## Technologies Used
+- Python
+
+## How to Use
+1. Run the application.
+2. Enter the desired length for the password.
+3. Specify the character types to include by selecting the corresponding checkboxes.
+4. Click the "Generate Password" button.
+5. The generated password will be displayed on the screen.
--- script.js
+++ script.js
@@ -1,14 +1,67 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Generate a random password based on user criteria
+function generatePassword() {
+  // Prompt for password length
+  var length = parseInt(
+    prompt('Enter the desired password length (between 8 and 128 characters):')
+  );
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Validate password length
+  while (isNaN(length) || length < 8 || length > 128) {
+    length = parseInt(
+      prompt(
+        'Invalid length! Please enter a number between 8 and 128 for the password length:'
+      )
+    );
+  }
 
-  passwordText.value = password;
+  // Prompt for character types
+  var includeLowercase = confirm('Include lowercase characters?');
+  var includeUppercase = confirm('Include uppercase characters?');
+  var includeNumeric = confirm('Include numeric characters?');
+  var includeSpecial = confirm('Include special characters?');
 
+  // Validate at least one character type is selected
+  while (
+    !includeLowercase &&
+    !includeUppercase &&
+    !includeNumeric &&
+    !includeSpecial
+  ) {
+    alert('At least one character type must be selected!');
+    includeLowercase = confirm('Include lowercase characters?');
+    includeUppercase = confirm('Include uppercase characters?');
+    includeNumeric = confirm('Include numeric characters?');
+    includeSpecial = confirm('Include special characters?');
+  }
+
+  // Define the characters to be included based on selected criteria
+  var characters = '';
+  if (includeLowercase) {
+    characters += 'abcdefghijklmnopqrstuvwxyz';
+  }
+  if (includeUppercase) {
+    characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+  if (includeNumeric) {
+    characters += '0123456789';
+  }
+  if (includeSpecial) {
+    characters += '!@#$%^&*()-_=+';
+  }
+
+  var password = '';
+  for (var i = 0; i < length; i++) {
+    // Generate a random index to pick a character from the characters string
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    // Append the randomly selected character to the password
+    password += characters.charAt(randomIndex);
+  }
+
+  return password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Generate password when the button is clicked
+document.getElementById('generate').addEventListener('click', function () {
+  var generatedPassword = generatePassword();
+  document.getElementById('password').value = generatedPassword;
+});
